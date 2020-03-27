870272c27af2e82b7a21d07c46a9cb95f502862a
Michael <mhunt429@gmail.com>
2020-03-27 14:24:05 -0400

    Initial Commit

---
+++ publisher/Controllers/ValuesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Text.Json;
+
+namespace publisher.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Index()
+        {
+            return new string[] { "Value1", "Value2" };
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]JsonElement body) { 
+            string json = System.Text.Json.JsonSerializer.Serialize(body);
+             return Ok("{\"success\": \"true\"}");
+        }
+
+
+    }
+}
---
+++ worker/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Text;
+using Newtonsoft.Json;
+namespace worker
+{
+    class Program
+    {
+        public static async Task PostMessage(string postData)
+        {
+            var json = JsonConvert.SerializeObject(postData);
+            var content = new StringContent(json, UnicodeEncoding.UTF8, "application/json");
+            
+            using (var httpClientHandler = new HttpClientHandler())
+            {
+                httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; };
+                using (var client = new HttpClient(httpClientHandler))
+                {
+                    var result = await client.PostAsync("https://localhost:5001/api/values", content);
+                    string resultContent = await result.Content.ReadAsStringAsync();
+                    Console.WriteLine("Server returned " + resultContent);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Posting a Message!");
+            PostMessage("Hello World").Wait();
+        }
+    }
+}
